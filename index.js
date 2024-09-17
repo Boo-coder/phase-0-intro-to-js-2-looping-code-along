abd22a6f8c253bf510f04d6d736e327d44a876de
Boo-coder <Bouna112@yahoo.com>
2024-09-17 01:49:54 -0400

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const writeCards = (names,event) => {
+    const messages = []
+    for(let i = 0; i < names.length;i++) {
+        
+           const newMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+           messages.push(newMessage)
+    }
+   
+    return messages;
+}
+
+writeCards;
+
+
+
+function countDown() {
+  for (let i = 10; i  >= 0; i = i - 1) {
+    console.log(i);
+  }
+}  
+  countDown;
